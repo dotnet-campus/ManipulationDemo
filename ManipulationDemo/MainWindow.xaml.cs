1fe93a276ac49198e24eda386e3bf5e1e061a966
lindexi <lindexi_gd@163.com>
2023-11-21 14:15:05 +0800

    尝试手动拿触摸设备

--- ManipulationDemo/MainWindow.xaml.cs
+++ ManipulationDemo/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Threading;
 using ManipulationDemo.Properties;
 using System.Management;
 using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Win32;
@@ -28,6 +29,8 @@ namespace ManipulationDemo
         {
             InitializeComponent();
             this.RemoveIcon();
+
+            Loaded += MainWindow_Loaded;
 
             var args = Environment.GetCommandLineArgs();
             if (args.Contains("--startup"))
@@ -90,6 +93,21 @@ namespace ManipulationDemo
             catch (Exception e)
             {
                 // 忽略
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var typeFromClsid = Type.GetTypeFromCLSID(new Guid("A5B020FD-E04B-4e67-B65A-E7DEED25B2CF"));
+            object comObject = Activator.CreateInstance(typeFromClsid);
+
+            var manager = comObject as ITabletManager;
+            manager!.GetTabletCount(out var tabletCount);
+            for (uint i = 0; i < tabletCount; i++)
+            {
+                manager.GetTablet(i, out var tablet);
+
+                tablet.GetName(out var name);
             }
         }
 
---
+++ ManipulationDemo/TabletManager/ITabletManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using HRESULT = System.Int32;
+
+namespace ManipulationDemo;
+
+[ComImport, Guid("764DE8AA-1867-47C1-8F6A-122445ABD89A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface ITabletManager
+{
+    int GetDefaultTablet(out ITablet ppTablet);
+    int GetTabletCount(out ulong pcTablets);
+    int GetTablet(ulong iTablet, out ITablet ppTablet);
+}
+
+[ComImport, Guid("1CB2EFC3-ABC7-4172-8FCB-3BC9CB93E29F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface ITablet //: IUnknown
+{
+    HRESULT GetDefaultContextSettings();
+    HRESULT CreateContext();
+    HRESULT GetName([MarshalAs(UnmanagedType.LPWStr)] out string ppwszName);
+    //HRESULT GetMaxInputRect(out RECT prcInput);
+    //HRESULT GetHardwareCaps(out uint pdwCaps);
+    //HRESULT GetPropertyMetrics([In] ref Guid rguid, out PROPERTY_METRICS pPM);
+    //HRESULT GetPlugAndPlayId([MarshalAs(UnmanagedType.LPWStr)] out string ppwszPPId);
+    //HRESULT GetCursorCount(out uint pcCurs);
+    // HRESULT GetCursor(uint iCur, out ITabletCursor ppCur);
+}
