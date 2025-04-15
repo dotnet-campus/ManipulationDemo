9bf929a5422a1945a55c8a38cd91dfa8a23e8ea2
lindexi <lindexi_gd@163.com>
2025-04-15 19:29:55 +0800

    添加 Edid 处理相关方法

---
+++ ManipulationDemoCpfX11/Utils/EDIDParser/ReadEdidInfoResult.cs
@@ -0,0 +1,7 @@
+﻿namespace ManipulationDemoCpfX11.Utils;
+
+public readonly record struct ReadEdidInfoResult(bool IsSuccess, string ErrorMessage, EdidInfo EdidInfo)
+{
+    public static ReadEdidInfoResult Success(EdidInfo edidInfo) => new ReadEdidInfoResult(true, "Success", edidInfo);
+    public static ReadEdidInfoResult Fail(string errorMessage) => new ReadEdidInfoResult(false, errorMessage, default);
+}
